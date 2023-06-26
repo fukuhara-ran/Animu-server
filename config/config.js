98b8e8f1ea3017f001bc1517419127f182397bd0
Reksa Panduasa <panduasareksa@students.amikom.ac.id>
2023-06-26 21:42:34 +0700

    adding sensitive content

---
+++ config/config.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    dialect: process.env.DB_DIALECT,
+    host: process.env.DB_HOST,
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    dialect: process.env.DB_DIALECT,
+    host: process.env.HOSTNAME,
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    dialect: process.env.DB_DIALECT,
+    host: process.env.HOSTNAME,
+  },
+};
